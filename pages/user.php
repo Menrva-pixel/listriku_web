426f08c7d885f6956cdc00eb977b4698953bbbbd
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-07-12 23:45:43 +0700

    update
    fetching data from database is now working

--- pages/user.php
+++ pages/user.php
@@ -19,6 +19,20 @@ function isUserPage() {
     return isset($_SESSION['privilege']) && $_SESSION['privilege'] == 'Pelanggan';
 }
 
+function getPenggunaanListrik($user_id) {
+    global $conn;
+    $query = "SELECT * FROM penggunaan_listrik WHERE user_id='$user_id' ORDER BY tahun DESC, bulan DESC";
+    $result = mysqli_query($conn, $query);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+function getTagihanListrik($user_id) {
+    global $conn;
+    $query = "SELECT * FROM tagihan_listrik WHERE user_id='$user_id' ORDER BY tahun DESC, bulan DESC";
+    $result = mysqli_query($conn, $query);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
 // Redirect to login page if not logged in or not a user
 if (!isset($_SESSION['username']) || !isUserPage()) {
     header('Location: login.php');
@@ -28,13 +42,19 @@ if (!isset($_SESSION['username']) || !isUserPage()) {
 // Get user data
 $username = $_SESSION['username'];
 $user = getUserFromDatabase($username);
+
+// Get penggunaan listrik data
+$penggunaan_listrik = getPenggunaanListrik($user['user_id']);
+
+// Get tagihan listrik data
+$tagihan_listrik = getTagihanListrik($user['user_id']);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>User Page</title>
-    <!-- Include CSS stylesheets and other necessary scripts -->
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <h1>Welcome, <?php echo $user['username']; ?></h1>
@@ -43,23 +63,28 @@ $user = getUserFromDatabase($username);
     <table>
         <thead>
             <tr>
-                <th>Month</th>
-                <th>Amount</th>
+                <th>No</th>
+                <th>Bulan</th>
+                <th>Tahun</th>
+                <th>Jumlah Meter</th>
+                <th>Tarif per kWh</th>
+                <th>Total Tagihan</th>
+                <th>Status</th>
             </tr>
         </thead>
         <tbody>
             <!-- Loop through tagihan listrik data and display rows -->
-            <?php
-            $query = "SELECT * FROM tagihan_listrik WHERE user_id = '{$user['id']}'";
-            $result = mysqli_query($conn, $query);
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td>' . $row['bulan'] . '</td>';
-                echo '<td>' . $row['total_tagihan'] . '</td>';
-                echo '</tr>';
-            }
-            ?>
+            <?php foreach ($tagihan_listrik as $index => $row): ?>
+            <tr>
+                <td><?php echo $index + 1; ?></td>
+                <td><?php echo $row['bulan']; ?></td>
+                <td><?php echo $row['tahun']; ?></td>
+                <td><?php echo $row['jumlah_meter']; ?></td>
+                <td><?php echo number_format($row['tarif_per_kwh'], 2); ?></td>
+                <td><?php echo number_format($row['total_tagihan'], 2); ?></td>
+                <td><?php echo $row['status']; ?></td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 
@@ -68,24 +93,26 @@ $user = getUserFromDatabase($username);
     <table>
         <thead>
             <tr>
-                <th>Month</th>
-                <th>Usage</th>
+                <th>No</th>
+                <th>Bulan</th>
+                <th>Tahun</th>
+                <th>Meter Awal</th>
+                <th>Meter Akhir</th>
             </tr>
         </thead>
         <tbody>
             <!-- Loop through penggunaan listrik data and display rows -->
-            <?php
-            $query = "SELECT * FROM penggunaan_listrik WHERE user_id = '{$user['id']}'";
-            $result = mysqli_query($conn, $query);
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td>' . $row['month'] . '</td>';
-                echo '<td>' . $row['usage'] . '</td>';
-                echo '</tr>';
-            }
-            ?>
+            <?php foreach ($penggunaan_listrik as $index => $row): ?>
+            <tr>
+                <td><?php echo $index + 1; ?></td>
+                <td><?php echo $row['bulan']; ?></td>
+                <td><?php echo $row['tahun']; ?></td>
+                <td><?php echo $row['meter_awal']; ?></td>
+                <td><?php echo $row['meter_akhir']; ?></td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </body>
 </html>
+
