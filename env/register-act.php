0decad515532556ef5956aae128a7472e0cdcf88
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-07-16 10:57:12 +0700

    new registration method

--- env/register-act.php
+++ env/register-act.php
@@ -7,21 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $alamat = $_POST["alamat"];
     $no_telp = $_POST["no_telp"];
     $email = $_POST["email"];
-    $privilege = "Pelanggan"; // Atur privilege sesuai kebutuhan
+    $privilege = "Pelanggan";
     
+    // Generate angka unik acak sebagai user_id
+    $user_id = uniqid();
+
     // Query untuk memasukkan data registrasi ke tabel users
-    $sql = "INSERT INTO users (username, password, alamat, no_telp, email, privilege)
-            VALUES ('$username', '$password', '$alamat', '$no_telp', '$email', '$privilege')";
+    $sql = "INSERT INTO users (user_id, username, password, alamat, no_telp, email, privilege)
+            VALUES ('$user_id', '$username', '$password', '$alamat', '$no_telp', '$email', '$privilege')";
 
     if ($conn->query($sql) === TRUE) {
-        // Ambil user_id baru yang telah terdaftar
-        $user_id = $conn->insert_id;
-
-        // Set session dengan user_id
         session_start();
         $_SESSION["user_id"] = $user_id;
-
-        // Redirect ke halaman form registrasi rumah
+        
         header('Location: ../auth/register-house.php');
         exit();
     } else {
